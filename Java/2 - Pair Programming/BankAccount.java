d698f45db29e36c19966d05fd33076cbde679dc7
Sumayah-91 <sumayahh18@gmail.com>
2024-12-18 13:28:19 +0000

    Added BankAccount class

---
+++ Java/2 - Pair Programming/BankAccount.java
@@ -0,0 +1,35 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class BankAccount {
+
+    private static int nextAccountNumber = 100000;
+
+    private String customerName;
+    private int accountNumber;
+    private double balance;
+
+    public BankAccount(String customerName, double balance) {
+        this.accountNumber = nextAccountNumber();
+        this.customerName = customerName;
+        this.balance = balance;
+    }
+
+    private int generateAccountNumber() {
+        return nextAccountNumber++;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public double withdraw(double amount) {
+        return this.balance -= amount;
+    }
+}
